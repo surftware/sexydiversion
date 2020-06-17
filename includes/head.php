a2fa3e7cec28449475362f5e1cb61b064a6e403a
Armando <armando.moreno.tolentino@gmail.com>
2020-06-17 13:14:13 -0500

    Open Graphs Facebook Meta

--- includes/head.php
+++ includes/head.php
@@ -14,6 +14,18 @@
     <link rel="stylesheet" type="text/css" href="css/animate/animate.css">
     <link rel="stylesheet" type="text/css" href="css/featherlight/featherlight.css" />
     <link rel="stylesheet" type="text/css" href="css/featherlight/featherlight.gallery.css" />
+    <!--Open Graphs Facebook Meta  -->
+        <meta property="og:locale" content="es_MX" />
+    <meta property="og:image/jpeg"  content="website" />
+    <meta property="og:title" content="sexydiversion, Sex Shop." />
+    <meta property="og:description"
+            content="Venta de productos para +18, tenemos gran variedad. No contamos con cabina. Precios accesibles para cualquier producto. Calidad e higiene." />
+    <meta property="og:url" content="http://www.surftware.company">
+    <meta property="og:site_name" content="Surftware">
+    <meta property="og:image" content="images/reparacion-computadoras1.jpg" />
+    <meta property="og:image" content="images/reparacion-computadoras2.jpg" />
+    <meta property="og:image" content="images/creacion-paginas-web.png" />
+    <meta property="og:image" content="images/welcome.png" />
     <!-- Custom Template CSS -->
     <link rel="stylesheet" href="css/style.css">
 </head>
