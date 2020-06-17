e7a24dd06b86ab52cde0093a67df953cb7e2bd35
Armando <armando.moreno.tolentino@gmail.com>
2020-06-17 13:21:04 -0500

    https://www.sexydiversion.com.mx

--- includes/head.php
+++ includes/head.php
@@ -20,7 +20,7 @@
     <meta property="og:title" content="sexydiversion, Sex Shop." />
     <meta property="og:description"
             content="Venta de productos para +18, tenemos gran variedad. No contamos con cabina. Precios accesibles para cualquier producto. Calidad e higiene." />
-    <meta property="og:url" content="http://www.surftware.company">
+    <meta property="og:url" content="https://www.sexydiversion.com.mx">
     <meta property="og:site_name" content="Surftware">
     <meta property="og:image" content="images/reparacion-computadoras1.jpg" />
     <meta property="og:image" content="images/reparacion-computadoras2.jpg" />
