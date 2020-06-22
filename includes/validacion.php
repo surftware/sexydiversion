85b25dc847fc9a5794cb3abc81669a31764e6164
Armando <armando.moreno.tolentino@gmail.com>
2020-06-22 14:03:54 -0500

    merje de rama front con ramajose

--- includes/validacion.php
+++ includes/validacion.php
@@ -12,8 +12,8 @@ if (!empty($_POST)) {
     $texto              = $_POST["mensaje"];
     $correo             = $_POST["correo"];
     $recaptchaPrueba    = '22222222';
-        //if (!empty($_POST["g-recaptcha-response"])) {
-        if (!empty($recaptchaPrueba)) {
+        if (!empty($_POST["g-recaptcha-response"])) {
+        //if (!empty($recaptchaPrueba)) {
         //echo "Se acepta recatcha";
             if ($nombre == "" || $telefono== "" || $correo == "" || $direccion == "" || $texto == "" || strlen($telefono) != 10 || is_valid_email($correo) != true ) 
             {
