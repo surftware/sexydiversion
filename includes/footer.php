f1d591bbf238d53454271c65970343b096d3d8e8
dayaxis <diaz_teran@hotmail.com>
2020-06-29 14:15:36 -0500

    correcion en el footer letras abajo del logotipo

--- includes/footer.php
+++ includes/footer.php
@@ -6,7 +6,11 @@
                 <a class="navbar-brand" href="#">
                     <div> <img src="assets/img/global/logo.png" height="200" width="211"
                             alt="Sexy-Diversion-Sex-Shop-logo">
-                        <p class="footer-logo text-white">Sex Shop <p>| Sexy Diversión</p>
+                        <a class="footer-logo ">
+                            
+                        <spam class= "text-white"> Sex Shop </spam>  <spam>| Sexy Diversión </spam>
+                        
+                        </a>
                         
 
 
@@ -19,16 +23,16 @@
                 <h4 class="footer-col-title icou-brand-title mt-3">Cat&aacutelogos</h4>
                 <div class="services-footer d-flex justify-content-end clearfix">
                     <ul class="list-unstyled col-sm-6 mr-auto p-0">
-                        <li><a href="catalogo/recargables.pdf">Vibradores Recargables</a></li>
-                        <li><a href="catalogo/estimulantes.pdf">Estimulantes</a></li>
-                        <li><a href="catalogo/juegos.pdf">Juegos</a></li>
-                        <li><a href="catalogo/anal.pdf">Juguetes Anales</a></li>
-                        <li><a href="catalogo/recargables.pdf">Alargadores</a></li>
-                        <li><a href="catalogo/balas.pdf">Balas</a></li>
-                        <li><a href="catalogo/doble.pdf">Vibradores Dobles</a></li>
-                        <li><a href="catalogo/anillos.pdf">Anillos</a></li>
-                        <li><a href="catalogo/masturbadores.pdf">Masturbadores</a></li>
-                        <li><a href="catalogo/fetish.pdf">Fetish</a></li>
+                        <li><a href="catalogo/recargables.pdf" target="_blank">Vibradores Recargables</a></li>
+                        <li><a href="catalogo/estimulantes.pdf" target="_blank">Estimulantes</a></li>
+                        <li><a href="catalogo/juegos.pdf" target="_blank">Juegos</a></li>
+                        <li><a href="catalogo/anal.pdf" target="_blank">Juguetes Anales</a></li>
+                        <li><a href="catalogo/recargables.pdf" target="_blank">Alargadores</a></li>
+                        <li><a href="catalogo/balas.pdf" target="_blank">Balas</a></li>
+                        <li><a href="catalogo/doble.pdf" target="_blank">Vibradores Dobles</a></li>
+                        <li><a href="catalogo/anillos.pdf" target="_blank">Anillos</a></li>
+                        <li><a href="catalogo/masturbadores.pdf" target="_blank">Masturbadores</a></li>
+                        <li><a href="catalogo/fetish.pdf" target="_blank">Fetish</a></li>
                         
 
                     </ul>
@@ -45,7 +49,7 @@
                     <p class="icou-t-white address-item"><i class="fa fa-envelope-open" aria-hidden="true"></i>
                         ventas@sexydiversion.com</p>
                     <a href="#map">
-                    <p>
+                    <p class="footer-logo text-white">
                     Hagan su pedido
                     </p>
                     </a>
